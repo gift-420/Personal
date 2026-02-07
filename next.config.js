6a92060044c04249882e106318f22b5589af1510
gift-420 <126797772+gift-420@users.noreply.github.com>
2026-02-08 03:46:26 +0600

    next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com'],
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
