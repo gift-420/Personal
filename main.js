e1f91376870d3f590e6093e01b6c63469daeb038
gift-420 <126797772+gift-420@users.noreply.github.com>
2026-02-10 13:24:48 +0600

    main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+new Typed(".typed", {
+  strings: [
+    "Diploma Electronics Engineer",
+    "Circuit Designer",
+    "Power Electronics",
+    "Embedded Systems"
+  ],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: true
+});
